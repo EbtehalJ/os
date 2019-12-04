7fe4a054659d9bb95157570cabdf6dcbadca790e
ebteh_000 <ebteh_000@Ebtehal>
2019-12-04 22:49:23 +0300

    Fourth upload (NetBeans IDE)

---
+++ download.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>BDB| Download</title>
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- Custom styles for this template -->
+    <link href="css/modern-business.css" rel="stylesheet">
+
+    <!-- Temporary navbar container fix -->
+    <style>
+    .navbar-toggler {
+        z-index: 1;
+    }
+    
+    @media (max-width: 576px) {
+        nav > .container {
+            width: 100%;
+        }
+    }
+    </style>
+    <style>
+    .errorWrap {
+    padding: 10px;
+    margin: 0 0 20px 0;
+    background: #fff;
+    border-left: 4px solid #dd3d36;
+    -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+    box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+}
+.succWrap{
+    padding: 10px;
+    margin: 0 0 20px 0;
+    background: #fff;
+    border-left: 4px solid #5cb85c;
+    -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+    box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+}
+    </style>
+
+</head>
+
+<body>
+
+    <?php include('includes/header.php');?>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-- Page Heading/Breadcrumbs -->
+        <h1 class="mt-4 mb-3">Download</h1>
+
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item">
+                <a href="index.php">Home</a>
+            </li>
+			<li class="breadcrumb-item active">About</li>
+            <li class="breadcrumb-item active">Download</li>
+        </ol>
+
+        <!-- Content Row -->
+        <div class="row">
+            <!-- Map Column -->
+              <div class="col-lg-8 mb-4">
+                        <p>
+            You can find the source code of our website on a github so you can download it and follow the instructions. <br/>
+            
+            <b>Project Name:</b>Blood Donor Bank<br/>
+            <b>Language Used:</b> PHP <br/>
+            <b>Database:</b> My SQL <br/>
+            <b>User Interface Design:</b> HTML, AJAX,JQUERY,JAVASCRIPT <br/>
+            <b>Web Browser:</b> Mozilla, Google Chrome, IE8,OPERA <br/>
+            <b>Software:</b> XAMPP Server Blood Bank & Donor <br/>
+			<b>Source code:</b><a class="navbar-brand" href="https://github.com/EbtehalJ/OS"> Blood Donor Bank Github</a><br/>
+           </p>   
+            
+           <h4>Visitor Feature’s: </h4>
+            <p>Easy to use <br/>
+            Blood Donor Registration System <br/>
+            Blood Group and location-based search engine <br/>
+            Donor Details Information <br/>
+            Contact us Inquiry <br/>
+            Development Inquiry <br/>
+            Download Inquiry <br/> 
+            </p>
+            
+            <h4> Admin Feature’s: </h4>
+            <p> Manage Blood Group(add, delete) <br/> 
+                Add and Delete Donor Information <br/> 
+                Enable and Disable Donors <br/> 
+                Manage Website Pages <br/> 
+                Manage to Contact us Queries <br/> 
+                Update the contact us Info <br/> 
+                Admin Dashboard <br/> 
+            </p>
+            </div>
+
+            <!-- Contact Details Column -->
+
+            </div>
+        </div>
+        <!-- /.row -->
+
+
+    </div>
+	
+    <!-- /.container -->
+<?php include('includes/footer.php');?>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/tether/tether.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Contact form JavaScript -->
+    <!-- Do not edit these files! In order to set the email address and subject line for the contact form go to the bin/contact_me.php file. -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+
+</body>
+
+</html>
